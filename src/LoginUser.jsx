c1403c586d08433db11f502b78e27a3535f1a7b3
gulu43 <gulamnabi9th@gmail.com>
2025-05-25 16:47:43 +0530

    login connected to backend

--- src/LoginUser.jsx
+++ src/LoginUser.jsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from 'react';
 import './LoginUser.css'
 
 export function LoginUser() {
+  const PORT = import.meta.env.VITE_PORT || 8000;
+  let [data, setData] = useState({
+    email: "",
+    password: ""
+  })
+  let sendDataForLoginFn = async ()=>{
+    // "http://localhost:5250/user/login"
+    //  here
+    fetch(`http://localhost:${PORT}/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data) 
+    })
+    .then((res)=>{
+      console.log("You are Logined Succesfully",res)
+    })
+    .catch((error)=>{
+      console.log("error while sending data",error?.message)
+    })
+  }
+  
   return (
     <>
 
@@ -9,14 +33,25 @@ export function LoginUser() {
       <div className='fieldsContainer'>
         <div className='emailLableAndTextContainer'>
           <label htmlFor='emailInpId' className='emailLabel'>Email: </label>
-          <input type="email" name="" id="emailInpId" className='emailInp' />
+          <input type="email" name="" id="emailInpId" className='emailInp' onChange={(e)=>{
+            setData((prev)=>({
+              ...prev,
+              email: e.target.value 
+            }))
+            
+            }} />
         </div>
         <div className='passwordLableAndTextContainer'>
           <label htmlFor='passwordInpId' className='passwordLabel'>Password: </label>
-          <input type="password" name="" id="passwordInpId" className='passwordInp' />
+          <input type="password" name="" id="passwordInpId" className='passwordInp' onChange={(e)=>{
+            setData((prev)=>({
+              ...prev,
+              password: e.target.value
+            }))
+          }} />
         </div>
         <div>
-          <button className='loginBtn'>Log-in</button>
+          <button className='loginBtn' onClick={ sendDataForLoginFn }>Log-in</button>
         </div>
 
       </div>
