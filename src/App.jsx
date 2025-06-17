da5eeb3005428188dd06eb9ba27f67f29e26e9de
gulu43 <gulamnabi9th@gmail.com>
2025-06-17 13:28:23 +0530

    added loginout frounted and tost for message

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route, useNavigate, useLocation, Navigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import { LoginUser } from './LoginUser.jsx';
 import { SignupUser } from './SignupUser.jsx';
 import { RefreshUser } from './RefreshUser.jsx';
+import { LogoutUser } from './LogoutUser.jsx';
 import './App.css';
 
 export default function App() {
   const navigate = useNavigate();       // allows URL changes (like navigate("/login"))
   const location = useLocation();       // tells us current path (like "/login" or "/register")
-4
+
   const isLogin = location.pathname === "/user/login"; // boolean for checked state
 
   return (
@@ -30,10 +32,12 @@ export default function App() {
           <Route path="/user/login" element={<LoginUser />} />
           <Route path="/user/register" element={<SignupUser />} />
           {/* <Route path="/user/udata" element={<LoginUser />} /> */}
+          <Route path='/user/logout' element={ <LogoutUser/> }/>
           <Route path="/user/auth/refresh" element={< RefreshUser />} />
           <Route path="/" element={<Navigate to="/user/login" />} /> {/* redirect root to login */}
           <Route path="*" element={<Navigate to="/user/login" />} /> 
         </Routes>
+        <ToastContainer position="top-center" autoClose={4000} theme='dark' />
       </div>
     </div>
   );
---
+++ src/LogoutUser.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Router, Route, useNavigate, Outlet } from 'react-router-dom';
+import './LogoutUser.css'
+import { toast } from 'react-toastify';
+export function LogoutUser() {
+
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    const BACKENDURL = import.meta.env.VITE_BACKEND_URL;
+
+
+    const logoutUserFn = async () => {
+        try {
+            const response = await fetch(`${BACKENDURL}/user/logout`, {
+                method: 'POST',
+                credentials: 'include'
+            })
+            const result = await response.json();
+            console.log("result of logout-> " + result);
+            for (const key in result) {
+                console.log(`${key}: ${result[key]}`);
+            }
+
+            if (result.ok) {
+                // setMessage(result.message || "Logout successfully!")
+                toast.error(result.message || "Logout successfully!");
+                navigate("/user/login");
+
+            } else {
+                toast.error(result.message || "Logout Failed!" )
+                // setMessage(result.message || "Logout Failed!")
+            }
+        } catch (error) {
+            toast.error(error?.message || error || "Logout Failed! in bitween")
+            console.log("logout user error "+error);
+            
+        }
+    }
+
+
+    return (
+        <>
+            <h1>Logout page</h1><br/>
+            <button className='logoutBtn' onClick={logoutUserFn}>log out</button>
+            {/* <div>message: {message}</div> */}
+        </>
+    );
+}
